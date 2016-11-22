b79001e0b2726d7ca6f16eaa717cfc627f8cfe30
Eder <edermartioli@gmail.com>
2016-11-22 10:22:38 -0200

    coding ...

--- src/exoplanets.py
+++ src/exoplanets.py
@@ -12,6 +12,7 @@
 
 class planet :
     'Common base class for a planet'
+    
     def __init__(self, Id):
         self.id=Id
 
@@ -22,26 +23,80 @@ class planet :
             print "Error: cannot retrieve exoplanet: ",self.id
             exit()
 
-    def setPhysical(self, Mass, Radius) :
-        self.mass = Mass
-        self.radius = Radius
+    def setPhysical(self, Mp, Rp) :
+
+        """ Set physical parameters of planet.
+            
+            Parameters
+            ----------
+            
+            Mp    : The mass of the planet    [Mjup]
+            
+            Rp    : The radius of the planet  [Rjup]
+            
+            """
+        
+        self.mass = Mp
+        self.radius = Rp
 
     def setOrbit(self, a, e, i, o, w, t0, P) :
-        self.semimajoraxis = a  # AU
-        self.eccentricity = e   # 0 - 1
-        self.inclination = i    # degrees
-        self.longascnode = o    # degrees
-        self.argperiapse = w    # degrees
-        self.timeofperipass = t0 # BJD
-        self.orbitalperiod = P  # days
+        
+        """ Set orbital parameters of planet.
+            
+            Parameters
+            ----------
+            
+            a    : The semimajor axis of the orbit of the planet [AU]
+            
+            e    : The eccentricity                [-]
 
-    def setParentStar(self, mass, spctype) :
-        self.starmass = mass
-        self.spectraltype = spctype
+            i    : The orbital inclination         [deg]
+            
+            o    : The longitude of ascending node [deg]
+
+            w    : The argument of periapse        [deg]
+            
+            t0   : The time of periapse passage    [BJD]
+            
+            P    : The orbital period              [days]
+            
+            """
+        
+        self.semimajoraxis = a
+        self.eccentricity = e
+        self.inclination = i
+        self.longascnode = o
+        self.argperiapse = w
+        self.timeofperipass = t0
+        self.orbitalperiod = P
+
+    def setParentStar(self, Ms, Ls, SType) :
+        
+        """ Set parameters of parent star.
+            
+            Parameters
+            ----------
+            
+            Ms    : The mass of the parent star           [Msun]
+            
+            Ls    : The luminosity of the parent star     [Lsun]
+            
+            SType : The spectral type of the parent star  [OBAFGKM]
+            
+            """
+        
+        self.starmass = Ms
+        self.starluminosity = Ls
+        self.spectraltype = SType
 
     def loadExoplanetDataFromCatalog(self):
 
-    def loadSolarSystemPlanet(self) :
+        """ Retrieve system parameters from online catalogs
+            """
+
+    def loadSolarSystemPlanet(self, id) :
+        """ Load information for a solar system planet. 
+            """
         SolarSystemPlanets = 'mercury venus earth mars jupiter saturn uranus neptune'.split()
 
 
